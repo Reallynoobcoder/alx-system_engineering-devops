1807db997d01988b6fd2c5fac8b2e83ab083409a
Elimali hamza <elimal.hamza@gmail.com>
2023-12-22 06:42:10 -0800

    .

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
-# Install Flask version 2.1.0 using pip3
+# Install Flask version 2.1.0
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+  require  => Package['python3-pip']
+}
+
+package { 'python3-pip':
+  ensure => 'installed',
 }
