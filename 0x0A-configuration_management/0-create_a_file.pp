4254eb01d1520dab7c5546456fa04cfb056d33bc
Elimali hamza <elimal.hamza@gmail.com>
2023-12-22 04:11:40 -0800

    .

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 file { '/tmp/school':
   ensure   => 'file',
   mode     => '0744',
-  content  => 'I love Puppet',
   owner    => 'www-data',
   group    => 'www-data',
+  content  => 'I love Puppet',
 }
