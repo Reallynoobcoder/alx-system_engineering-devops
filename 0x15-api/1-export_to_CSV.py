b67597ab0d7f905552c87194f89d0cdc57492605
elimali hamza <elimal.hamza@gmail.com>
2024-01-29 10:49:33 -0800

    .

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,11 +6,13 @@ from sys import argv
 
 if __name__ == "__main__":
     employee_id = int(argv[1])
-    user = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}").json()
+    user = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{employee_id}").json()
     todos = requests.get("https://jsonplaceholder.typicode.com/todos",
                          params={"userId": employee_id}).json()
 
     with open(f"{employee_id}.csv", 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for task in todos:
-            writer.writerow([employee_id, user['username'], task['completed'], task['title']])
+            writer.writerow([employee_id, user['username'],
+                            task['completed'], task['title']])
