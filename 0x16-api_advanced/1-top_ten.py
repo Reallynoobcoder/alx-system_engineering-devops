c98ed30d2f25bf6ccf4c4628a921f131dd0d4ffe
El imali hamza <elimal.hamza@gmail.com>
2024-02-13 11:26:00 -0800

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
-import requests
 """
 queries the Reddit API and prints the titles of the
 first 10 hot posts listed for a given subreddit.
 """
+import requests
 
 
 def top_ten(subreddit):
     """
-    Prints the titles of the first 10 hot posts for a given subreddit
+    Get and print the titles of the first 10 hot posts for a given subreddit.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'MyCustomUserAgent/1.0'}
 
-    headers = {'User-Agent': 'Naka/1.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    respons = requests.get(url, headers=headers, allow_redirects=False)
-
-    if respons.status_code == 200:
-
-        data = respons.json()
+    if response.status_code == 200:
+        data = response.json()
         posts = data['data']['children']
 
         for post in posts[0:10]:
