3b13f619909536390042977e8f23d068acab9a7a
El imali hamza <elimal.hamza@gmail.com>
2024-02-13 11:02:19 -0800

    .

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
-import requests
 """
 Queries the Reddit API and returns the number of subscribers
 """
+import requests
 
 
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns the number of subscribers subreddit.
-    If an invalid subreddit is given, returns 0.
+    Retrieve the number of subscribers for a given subreddit.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'MyCustomUserAgent/1.0'}
 
-    headers = {'User-Agent': 'Naka/1.0'}
-
-    response = requests.get(url, headers, allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
         subscribers = data['data']['subscribers']
-
         return subscribers
     else:
         return 0
