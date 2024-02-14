19a3c2ace1a8d3be0a7a6b18288c6d846be7081c
El imali hamza <elimal.hamza@gmail.com>
2024-02-14 03:59:17 -0800

    .

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# Fix 500 Internal Server Error returned by the Apache server.
+exec { 'replace':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => ['/usr/bin', '/bin'],
+}
